c17a65073dfa09666b852724b832e28740200a31
Szabó Gergő <lest12110@gmail.com>
2025-03-16 21:27:56 +0100

    Refactor GPIO initialization and error handling in main loop for improved readability and structure

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 
 void init_gpio()
 {
-    gpio_init(LED);                                 // Initialize LED pin
-    gpio_set_dir(LED, GPIO_OUT);                    // Set LED pin as output
-    gpio_init(UPSIDE_DOWN_SENSOR);                  // Initialize GPIO10
-    gpio_set_dir(UPSIDE_DOWN_SENSOR, GPIO_IN);      // Set as input
+    gpio_init(LED);                            // Initialize LED pin
+    gpio_set_dir(LED, GPIO_OUT);               // Set LED pin as output
+    gpio_init(UPSIDE_DOWN_SENSOR);             // Initialize GPIO10
+    gpio_set_dir(UPSIDE_DOWN_SENSOR, GPIO_IN); // Set as input
     // Initialize virtual serial pins
     gpio_init(VIRTUAL_DATA_LINE);
     gpio_set_dir(VIRTUAL_DATA_LINE, GPIO_OUT);
@@ -15,8 +15,9 @@ void init_gpio()
     gpio_set_dir(VIRTUAL_ENABLE_LINE, GPIO_IN);
     gpio_init(VIRTUAL_NOTIFY_LINE);
     gpio_set_dir(VIRTUAL_NOTIFY_LINE, GPIO_OUT);
-    for (int i = 0; i < 10; i++) {
-        //blink led 
+    for (int i = 0; i < 10; i++)
+    {
+        // blink led
         gpio_put(LED, 1);
         sleep_ms(100);
         gpio_put(LED, 0);
@@ -35,15 +36,25 @@ void catch_error()
     }
 }
 
-int main() {
+int main()
+{
     init_gpio();
-    while (true) {
-        BME280Data data = readBME280();
-        bool upsideDown = isUpsideDown();
-        const std::string& datasummary = "Temperature: " + std::to_string(data.temperature) + " Pressure: " + std::to_string(data.pressure) + " Humidity: " + std::to_string(data.humidity) + " Upside Down: " + std::to_string(upsideDown);
-        int res = sendString(datasummary);
-        if (res != 0) {
-            //blink red
+    while (true)
+    {
+        try
+        {
+            BME280Data data = readBME280();
+            bool upsideDown = isUpsideDown();
+            const std::string &datasummary = "Temperature: " + std::to_string(data.temperature) + " Pressure: " + std::to_string(data.pressure) + " Humidity: " + std::to_string(data.humidity) + " Upside Down: " + std::to_string(upsideDown);
+            int res = sendString(datasummary);
+            if (res != 0)
+            {
+                // blink red
+                catch_error();
+            }
+        }
+        catch (const std::exception &e)
+        {
             catch_error();
         }
     }
